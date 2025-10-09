0d3372613548068ceba796aee79d0808857b2517
Студент группы IS <is@sielom.local>
2025-10-09 10:11:50 +0500

    A

--- django_pr/d_app/admin.py
+++ django_pr/d_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Employees)
+admin.site.register(Tables)
+admin.site.register(Costomers)
+admin.site.register(Menu)
+admin.site.register(Orders)
+admin.site.register(OrderItems)
+admin.site.register(Reservations)
+admin.site.register(Shifts)
